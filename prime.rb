8b975f34a835066583a2aef53d8c820907a4e0f9
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 21:47:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,12 @@
 # Add  code here!
-
-
-def prime?(integer)
-  test_divisors = (2..(integer - 1)).to_a
-  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true
+def prime?(number)
+  counter = 2
+  if number > 1
+    prime = (counter..(number-1)).to_a
+    prime.none? do |divide|
+      number % divide == 0
+    end
+  else
+    return false
+    end
 end
